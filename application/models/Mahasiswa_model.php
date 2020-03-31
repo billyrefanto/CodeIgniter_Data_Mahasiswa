3cd44a30362a537fec329f708951e3d63d091c79
Moch Billy Refanto <mochbillyrefanto@gmail.com>
2020-03-31 13:35:46 +0700

    Framework PHP Codeigniter

--- application/models/Mahasiswa_model.php
+++ application/models/Mahasiswa_model.php
@@ -3,4 +3,52 @@ class Mahasiswa_model extends CI_model {
     public function getAllMahasiswa(){
         return $query = $this->db->get('mahasiswa')->result_array();
     }
+    public function tambahDataMahasiswa(){
+        // $nama = $this->input->post('nama');
+		// $nim = $this->input->post('nim');
+		// $email = $this->input->post('email');
+		// $foto = $_FILES['image'];
+		// if ($foto='') {}else{
+		// 	$config['upload_path'] = './assets/foto';
+		// 	$config['allowed_types'] = 'jpg|jpeg|png|gif';
+
+		// 	$this->load->library('upload',$config);
+		// 	if (!$this->upload->do_upload('foto')) {
+		// 		echo "Gagal upload"; die();
+		// 	} else {
+		// 		$foto = $this->upload->data('file_name');
+		// 	}
+		// }
+
+		// $data = [
+		// 	'nama' => $nama,
+		// 	'pengarang' => $nim,
+		// 	'penerbit' => $email,
+		// 	'foto' => $foto
+        // ];
+
+		// $this->db->insert($data, 'mahasiswa');
+		// redirect('mahasiswa');
+	
+        $data = [
+            "nama" =>$this->input->post("nama",true),
+            "nim" => $this->input->post("nim",true),
+            "email" => $this->input->post("email",true),
+            "foto" => $this->input->post("image",true)
+        ];
+        $this->db->insert('mahasiswa',$data); 
+    }
+    public function detail($id){
+		$this->load->model('m_katalog');
+		$detail = $this->m_katalog->detail_data($id);
+		$data['detail'] = $detail;
+		$this->load->view('templates/header');
+		$this->load->view('templates/sidebar');
+		$this->load->view('detail',$data);
+		$this->load->view('templates/footer');
+	}
+    public function hapusDataMahasiswa($id){
+        $this->db->where('id',$id);
+        $this->db->delete('mahasiswa');
+    }
 }
--- application/views/Mahasiswa/index.php
+++ application/views/Mahasiswa/index.php
@@ -1,15 +1,82 @@
-<h1 class="text-justify">Data Mahasiswa D3RPLA-43-01</h1>
+<style>
+*{
+  margin:0px;
+  padding:0px;
+}
+.card-text{
+  text-align:center;
+}
+.nama{
+  text-align:center;
+}
+.email{
+  font-size:10pt;
+}
+.card{
+  margin:10px;
+  padding:10px;
+}
+.table{
+  margin:3px;
+}
+th{
+  text-align:center;
+}
+
+</style>
+<div class="container">
+  <?php if( $this->session->flashdata('flash') ) : ?>
+  <div class="row mt-3">
+    <div class="col-md-6">
+        <div class="alert alert-succes alert-dismissible fade show" role="alert">
+          Data Mahasiswa <strong>Berhasil!</strong> 
+          <?= $this->session->flashdata('flash') ?>.
+          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+    </div>
+  </div>
+  <?php endif; ?>
+
+<div class="row mt-3">
+  <div class="col-md-5">
+    <a href="<?= base_url(); ?>mahasiswa/tambah" class="btn btn-primary">Tambah mahasiswa</a>
+  </div>
+</div>
+<h1 class="text-justify" id="mahasiswa">Data Mahasiswa D3RPLA-43-01</h1>
 <table class="table">
     <tr>
       <th scope="col">Id</th>
+      <th scope="col">Foto</th>
       <th scope="col">Nama</th>
       <th scope="col">Nim</th>
+      <th scope="col">Email</th>
+      <th scope="col">Aksi</th>
     </tr>
-    <?php foreach ($mahasiswa as $data) : ?>
+    <?php foreach ($mahasiswa as $mhs) : ?>
     <tr>
-      <th scope="row"><?= $data['id'] ?></th>
-      <td scope="row"><?= $data['nama'] ?></td>
-      <td scope="row"><?= $data['nim'] ?></td>
+      <th scope="row"><?= $mhs['id'] ?></th>
+      <th scope="row"> <img src="<?php echo base_url(); ?>assets/foto/" width="150" height="150" alt="Foto mahasiswa"></th>
+      <td scope="row"><?= $mhs['nama'] ?></td>
+      <td scope="row"><?= $mhs['nim'] ?></td>
+      <td scope="row"><?= $mhs['email'] ?></td>
+      <td scope="row">
+        <a href="<?= base_url(); ?>mahasiswa/edit/<?= $mhs['id'];?>" class="badge badge-succes">Edit</a>
+        <a href="<?= base_url(); ?>mahasiswa/hapus/<?= $mhs['id'];?>" class="badge badge-danger" onclick="return confirm('Apakah anda yakin?');">hapus</a>
+      </td>
     </tr>
     <?php endforeach ?>
 </table>
+    <!-- <?php foreach ($mahasiswa as $data) : ?>
+    <div class="card" style="width: 18rem;">
+      <img src="..." class="card-img-top" alt="foto mahasiswa">
+      <div class="card-body">
+        <h5 class="card-title nama"><?= $data['nama'] ?></h5>
+        <p class="card-text"><?= $data['nim'] ?></p>
+        <p class="card-text email"><?= $data['email'] ?></p>
+        <button type="button" class="btn btn-primary" ><span class="oi oi-trash"></span>Edit</button>
+        <button type="button" class="btn btn-danger">Hapus</button>
+    </div>
+    <?php endforeach ?> -->
+</div>
